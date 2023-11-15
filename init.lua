796bf67d93e625f1fd93ca34f97b30e5f5323eec
sharedordaz <sharedordaz@live.com.mx>
2023-11-15 14:51:28 -0600

    First commit

---
+++ init.lua
@@ -0,0 +1,94 @@
+--SETTINGS
+vim.opt.nu = true
+vim.opt.guicursor = ""
+vim.opt.cursorline = true
+
+--vim.opt.mouse = "v" 
+vim.opt.mouse = "a"
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.swapfile = false 
+
+vim.opt.backupdir = "~/.cache/vim"
+
+--require'lspconfig'.pyright.setup{}
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+vim.g.mapleader = " "
+
+
+require("lazy").setup("plugins")
+
+--AFTER PLUGINS CHARGED
+
+vim.cmd.colorscheme("PaperColor")
+
+--ENABLE TO DO SET DEFAULT NEOTREES
+--vim.api.nvim_exec([[
+--	augroup Mygroup
+--	autocmd!
+--	autocmd BufNew * Neotree
+--	autocmd BufNewFile * Neotree
+--	augroup END]], false)
+
+function autosave_last_session()
+	vim.cmd "SessionSave autosave_neovim_last_session"
+end
+
+vim.api.nvim_exec([[
+	augroup MiGrupo
+	autocmd!
+	autocmd ExitPre * lua autosave_last_session()
+	augroup END]], false)
+-- vim.cmd "autocmd FileWritePost * echo 'hold' "
+
+
+
+----------VIM LSP KEYMAPS
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end})
+
---
+++ lua/plugins.lua
@@ -0,0 +1,125 @@
+return {
+{'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+	theme = "doom",
+	config = {    
+		header = {"",
+"",
+"                           ",
+" ██████   █████                  █████   █████  ███                 ",
+"░░██████ ░░███                   ░░███   ░░███  ░░░                  ",
+" ░███░███ ░███   █████   ██████  ░███    ░███  ████  █████████████  ",
+" ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███ ",
+" ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███ ",
+" ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███ ",
+" █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████",
+"░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░ ",
+"―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――",
+"",
+""		},
+
+center = {
+
+	{
+		icon = "◉",
+		desc = " Go to last session",
+		desc_hl = "String",
+		action = "SessionLoad autosave_neovim_last_session"
+	},
+	{
+		icon = "◉",
+		desc = " Edit init.lua",
+		desc_hl = "String",
+		action = "edit $MYVIMRC"
+	},
+	{
+		icon = "◉",
+		desc = " Edit Plugins",
+		desc_hl = "String",
+		action = "edit ~/.config/nvim/lua/plugins.lua"
+	}
+	}
+
+}
+}
+
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}},
+},
+--LSP SERVERS, MASON
+{"williamboman/mason.nvim",
+config = function()
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+
+end,
+},
+
+{"williamboman/mason-lspconfig.nvim"},
+{"neovim/nvim-lspconfig",
+nfig = function()
+lspconfig = require("lspconfig")
+lspconfig.pyright.setup{}
+end,
+},
+
+--DBSESSION
+    {'glepnir/dbsession.nvim',
+    cmd = { 'SessionSave', 'SessionDelete', 'SessionLoad'},
+      opts = {  --config --
+	      }},
+
+--NEOTREE
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+       "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+},
+--LUALINE
+{
+  'nvim-lualine/lualine.nvim',
+  config = function()
+   require("lualine").setup{
+	   options = { theme = "papercolor_dark"}
+
+   }
+  end,
+  dependencies = { 'nvim-tree/nvim-web-devicons'}
+},
+--THEMES
+{ "EdenEast/nightfox.nvim" },
+{"ku1ik/vim-monokai"},
+{"NLKNguyen/papercolor-theme"},
+
+--BARBAR (BUFFERS)
+{'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+
+}
+
