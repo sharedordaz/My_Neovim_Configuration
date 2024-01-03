2b1159bd029d84b39ecffff1cf69efa56fbcfefe
sharedordaz <sharedordaz@live.com.mx>
2024-01-03 02:30:25 -0600

    Added some shortcuts

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ Neotree toggle <<leader>>t
 ;l run lua command (:lua )
 ;h open help (:help)
 
+### Utility
+<leader>/ :noh
+
+### Buffers
+<S-h> :bp previous buffer
+<S-l> :bn next buffer
+<leader>b: show buffers
+<leader>bd: :bd delete buffer
+
--- lua/keys.lua
+++ lua/keys.lua
@@ -3,6 +3,8 @@ vim.g.mapleader = ','
 --TOGGLE NEOTREE with <leader> + n
 vim.api.nvim_set_keymap('n', '<leader>t', ':Neotree toggle<CR>', { noremap = true, silent = true })
 
+vim.api.nvim_set_keymap('i', '<leader>t', '<Esc>:Neotree toggle<CR>', { noremap = true, silent = true })
+
 
 --:SudaWrite with <leader> + w
 vim.api.nvim_set_keymap('n', '<leader>w', ':SudaWrite<CR>', {noremap=true, silent = false})
@@ -16,6 +18,7 @@ vim.api.nvim_set_keymap('n', ";h", ':LspInfo<CR>', {noremap=true, silent = false
 
 
 
+
 --Fast terminal shortcuts
 vim.api.nvim_set_keymap('n', ";;", ':', {noremap=true, silent = false})
 vim.api.nvim_set_keymap('n', ";l", ':lua<Space>', {noremap=true, silent = false})
@@ -24,8 +27,18 @@ vim.api.nvim_set_keymap('n', ";1", ':!', {noremap=true, silent = false})
 vim.api.nvim_set_keymap('n', ";h", ':help<Space>', {noremap=true, silent = false})
 
 --Utility
-vim.api.nvim_set_keymap('n', "<leader>n", ':noh<CR>', {noremap=true, silent = true})
+vim.api.nvim_set_keymap('n', "<leader>/", ':noh<CR>', {noremap=true, silent = true})
+
+--BUFFER ADMINISTRATION
+vim.api.nvim_set_keymap('n', "<S-h>", ':bp<CR>', {noremap=true, silent = true})
+vim.api.nvim_set_keymap('n', "<S-l>", ':bn<CR>', {noremap=true, silent = true})
+vim.api.nvim_set_keymap('i', "<S-h>", '<Esc>:bp<CR>', {noremap=true, silent = true})
+vim.api.nvim_set_keymap('i', "<S-l>", '<Esc>:bn<CR>', {noremap=true, silent = true})
 
 
 
+vim.api.nvim_set_keymap('n', "<leader>b", ':ls<CR>', {noremap=true, silent = true})
+
+vim.api.nvim_set_keymap('i', "<leader>bd", '<Esc>:bd!<CR>', {noremap=true, silent = true})
+vim.api.nvim_set_keymap('n', "<leader>bd", ':bd<CR>', {noremap=true, silent = true})
 
